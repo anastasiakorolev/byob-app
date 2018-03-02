c65d93b031a94361c362d8603302ecd743cf23b0
Anastasia Korolev <akorolev@placester.com>
2018-03-02 18:29:58 -0500

    Update directory and engine in index.js file to start server

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('index'))
+  .set('view engine', 'html')
+  .get('/', (req, res) => res.render('dev'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
