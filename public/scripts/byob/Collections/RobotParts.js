190abf2cacb03cb9dea1dd7e2d555802cc669837
Anastasia <anastasia.korolev22@gmail.com>
2018-04-12 20:38:05 -0400

    Add new models

--- public/scripts/byob/Collections/RobotParts.js
+++ public/scripts/byob/Collections/RobotParts.js
@@ -7,7 +7,7 @@
 			this.add(new Byob.Models.RobotPart({
 				type: 'head',
 				name: 'Iron Giant',
-				src: 'models/fuckthisshit.json',
+				src: 'models/IronGiantFullHead.json',
 				scale: 1,
 				posx: 0,
 				posy: 15,
@@ -61,7 +61,7 @@
 			this.add(new Byob.Models.RobotPart({
 				type: 'head',
 				name: 'Beauty Bot',
-				src: 'models/topFInal.json',
+				src: 'models/BeautyBotHead.json',
 				scale: 6,
 				posx: 0,
 				posy: 0,
@@ -70,7 +70,43 @@
 			this.add(new Byob.Models.RobotPart({
 				type: 'body',
 				name: 'Beauty Bot',
-				src: 'models/bottomFInal.json',
+				src: 'models/BeautyBotBody.json',
+				scale: 6,
+				posx: 0,
+				posy: 0,
+				posz: 0
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'rArm',
+				name: 'Beauty Bot',
+				src: 'models/BeautyBotRightArm.json',
+				scale: 6,
+				posx: 0,
+				posy: 0,
+				posz: 0
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'lArm',
+				name: 'Beauty Bot',
+				src: 'models/BeautyBotLeftArm.json',
+				scale: 6,
+				posx: 0,
+				posy: 0,
+				posz: 0
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'lLeg',
+				name: 'Beauty Bot',
+				src: 'models/BeautyBotLeftLeg.json',
+				scale: 6,
+				posx: 0,
+				posy: 0,
+				posz: 0
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'rLeg',
+				name: 'Beauty Bot',
+				src: 'models/BeautyBotRightLeg.json',
 				scale: 6,
 				posx: 0,
 				posy: 0,
