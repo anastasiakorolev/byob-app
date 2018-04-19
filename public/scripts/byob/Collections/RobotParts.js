b20f3ccfccab55d59737a0cef3dd748553bbe4b9
Anastasia <anastasia.korolev22@gmail.com>
2018-04-19 16:42:10 -0400

    add more robot parts

--- public/scripts/byob/Collections/RobotParts.js
+++ public/scripts/byob/Collections/RobotParts.js
@@ -5,7 +5,7 @@
 
 		initialize: function initialize() {
 			this.add(new Byob.Models.RobotPart({
-				type: 'head',
+				type: 'Head',
 				name: 'Iron Giant',
 				src: 'models/IronGiantFullHead.json',
 				srcCentered: 'models/IronGiantFullHeadCentered.json',
@@ -16,7 +16,7 @@
 				downloadLink: ['obj/IronGiantHead.obj', 'obj/IronGiantJaw.obj']
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'body',
+				type: 'Body',
 				name: 'Iron Giant',
 				src: 'models/IronGiantBody.json',
 				srcCentered: 'models/IronGiantBodyCentered.json',
@@ -27,7 +27,7 @@
 				downloadLink: 'obj/IronGiantBody.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'lArm',
+				type: 'Left Arm',
 				name: 'Iron Giant',
 				src: 'models/leftArmObj.json',
 				srcCentered: 'models/LeftArmObjCentered.json',
@@ -38,7 +38,7 @@
 				downloadLink: 'obj/LeftArmObj.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'rArm',
+				type: 'Right Arm',
 				name: 'Iron Giant',
 				src: 'models/rightArmObj.json',
 				srcCentered: 'models/RightArmObjCentered.json',
@@ -49,7 +49,7 @@
 				downloadLink: 'obj/RightArmObj.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'lLeg',
+				type: 'Left Leg',
 				name: 'Iron Giant',
 				src: 'models/leftLegObj.json',
 				srcCentered: 'models/LeftLegObjCentered.json',
@@ -60,7 +60,7 @@
 				downloadLink: 'obj/LeftLegObj.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'rLeg',
+				type: 'Right Leg',
 				name: 'Iron Giant',
 				src: 'models/rightLegObj.json',
 				srcCentered: 'models/RightLegObjCentered.json',
@@ -71,70 +71,136 @@
 				downloadLink: 'obj/RightLegObj.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'head',
-				name: 'Beauty Bot',
-				src: 'models/BeautyBotHead.json',
+				type: 'Head',
+				name: 'WALLE',
+				src: 'models/WALLE_E_HEAD_HOLLOW_1MM.json',
 				srcCentered: 'models/WALLE_E_HEAD_HOLLOW_1MM.json',
 				scale: 1,
 				posx: 0,
 				posy: 0,
 				posz: 0,
-				downloadLink: 'obj/BeautyBotHead.obj'
+				downloadLink: 'obj/WALLE_E_HEAD_HOLLOW_1MM.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'body',
-				name: 'Beauty Bot',
-				src: 'models/BeautyBotBody.json',
-				srcCentered: 'models/models/WALLE_E_BODY_HOLLOW_1MM.json',
+				type: 'Body',
+				name: 'WALLE',
+				src: 'models/WALLE_E_BODY_HOLLOW_1MM.json',
+				srcCentered: 'models/WALLE_E_BODY_HOLLOW_1MM.json',
 				scale: 1,
 				posx: 0,
 				posy: 0,
 				posz: 0,
-				downloadLink: 'obj/BeautyBotBody.obj'
+				downloadLink: 'obj/WALLE_E_BODY_HOLLOW_1MM.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'rArm',
-				name: 'Beauty Bot',
+				type: 'Right Arm',
+				name: 'WALLE',
 				src: 'models/WALLE_E_RIGHT_ARM_HOLLOW_1MM.json',
 				srcCentered: 'models/WALLE_E_RIGHT_ARM_HOLLOW_1MM.json',
 				scale: 1,
 				posx: 0,
 				posy: 0,
 				posz: 0,
-				downloadLink: 'obj/BeautyBotRightArm.obj'
+				downloadLink: 'obj/WALLE_E_RIGHT_ARM_HOLLOW_1MM.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'lArm',
-				name: 'Beauty Bot',
+				type: 'Left Arm',
+				name: 'WALLE',
 				src: 'models/WALLE_E_LEFT_ARM_HOLLOW_1MM.json',
 				srcCentered: 'models/WALLE_E_LEFT_ARM_HOLLOW_1MM.json',
 				scale: 1,
 				posx: 0,
 				posy: 0,
 				posz: 0,
-				downloadLink: 'obj/BeautyBotLeftArm.obj'
+				downloadLink: 'obj/WALLE_E_LEFT_ARM_HOLLOW_1MM.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'lLeg',
-				name: 'Beauty Bot',
+				type: 'Left Leg',
+				name: 'WALLE',
 				src: 'models/WALLE_E_LEFT_LEG_HOLLOW_1MM.json',
 				srcCentered: 'models/WALLE_E_LEFT_LEG_HOLLOW_1MM.json',
 				scale: 1,
 				posx: 0,
 				posy: 0,
 				posz: 0,
-				downloadLink: 'obj/BeautyBotLeftLeg.obj'
+				downloadLink: 'obj/WALLE_E_LEFT_LEG_HOLLOW_1MM.obj'
 			}));
 			this.add(new Byob.Models.RobotPart({
-				type: 'rLeg',
-				name: 'Beauty Bot',
+				type: 'Right Leg',
+				name: 'WALLE',
 				src: 'models/WALLE_E_RIGHT_LEG_HOLLOW_1MM.json',
 				srcCentered: 'models/WALLE_E_RIGHT_LEG_HOLLOW_1MM.json',
 				scale: 1,
 				posx: 0,
 				posy: 0,
 				posz: 0,
-				downloadLink: 'obj/BeautyBotRightLeg.obj'
+				downloadLink: 'obj/WALLE_E_RIGHT_LEG_HOLLOW_1MM.obj'
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'Head',
+				name: 'Plankton Bot',
+				src: 'models/CylinderBotBodyHead.json',
+				srcCentered: 'models/CylinderBotBodyHead.json',
+				scale: 1,
+				posx: 0,
+				posy: 0,
+				posz: 0,
+				downloadLink: 'obj/CylinderBotBodyHead.obj'
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'Body',
+				name: 'Plankton Bot',
+				src: 'models/CylinderBotBodyHead.json',
+				srcCentered: 'models/CylinderBotBodyHead.json',
+				scale: 1,
+				posx: 0,
+				posy: 0,
+				posz: 0,
+				downloadLink: 'obj/CylinderBotBodyHead.obj'
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'Left Arm',
+				name: 'Plankton Bot',
+				src: 'models/CylinderBotLeftArm.json',
+				srcCentered: 'models/CylinderBotLeftArm.json',
+				scale: 1,
+				posx: 0,
+				posy: 0,
+				posz: 0,
+				downloadLink: 'obj/CylinderBotLeftArm.obj'
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'Left Leg',
+				name: 'Plankton Bot',
+				src: 'models/CylinderBotLeftLeg.json',
+				srcCentered: 'models/CylinderBotLeftLeg.json',
+				scale: 1,
+				posx: 0,
+				posy: 0,
+				posz: 0,
+				downloadLink: 'obj/CylinderBotLeftLeg.obj'
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'Right Arm',
+				name: 'Plankton Bot',
+				src: 'models/CylinderBotRightArm.json',
+				srcCentered: 'models/CylinderBotRightArm.json',
+				scale: 1,
+				posx: 0,
+				posy: 0,
+				posz: 0,
+				downloadLink: 'obj/CylinderBotRightArm.obj'
+			}));
+			this.add(new Byob.Models.RobotPart({
+				type: 'Right Leg',
+				name: 'Plankton Bot',
+				src: 'models/CylinderBotRightLeg.json',
+				srcCentered: 'models/CylinderBotRightLeg.json',
+				scale: 1,
+				posx: 0,
+				posy: 0,
+				posz: 0,
+				downloadLink: 'obj/CylinderBotRightLeg.obj'
 			}));
 		},
 
