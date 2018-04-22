d3390d744f22af82ca76c7936c7be1266af136d3
Anastasia Korolev <akorolev@placester.com>
2018-04-22 14:25:07 -0400

    attempt

--- public/scripts/byob/Canvas.js
+++ public/scripts/byob/Canvas.js
@@ -163,19 +163,20 @@
 					console.log('model exists');
 					console.log(m.name);
 					console.log(m.type);
+					console.log(zip);
 					if (m.attributes.name === 'Iron Giant' && m.attributes.type === 'head') {
 						console.log(m.attributes);
 						console.log(m.attributes.downloadLink);
 						// downloadLinks.push(m.attributes.downloadLink[0]);
-						zip.add(m.attributes.downloadLink[0], m.name);
+						zip.push(m.attributes.downloadLink[0], m.name);
 						// downloadLinks.push(m.attributes.downloadLink[1]);
-						zip.add(m.attributes.downloadLink[1], m.name);
+						zip.push(m.attributes.downloadLink[1], m.name);
 					} else {
 						console.log('add link');
 						console.log(m.attributes);
 						console.log(m.attributes.downloadLink);
 						// downloadLinks.push(m.attributes.downloadLink);
-						zip.add(m.attributes.downloadLink, m.name);
+						zip.push(m.attributes.downloadLink, m.name);
 					}
 				}
 			});
